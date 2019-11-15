166532e15bee4593927fa0990ed5673fe61e4a1e
serhat-ulus <serhatulus@gmx.net>
2019-11-15 15:22:55 +0100

    DBConnection erstellt und in das Package
    de.hdm.itprojekt.server.db eingefügt.

---
+++ ITP_T4/src/de/hdm/itprojekt/server/db/DBConnection.java
@@ -0,0 +1,74 @@
+package de.hdm.itprojekt.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DBConnection {
+
+
+	/*
+	 * Die Klasse DBConnection wird nur einmal instanziiert. Wegen der Singleton Eigenschaft
+	 * Die Variable ist durch static nur einmal für sämtliche Instanzen vorhanden.
+	 */
+	private static Connection con = null;
+	
+	
+	private static String localUrl = "jdbc:mysql://localhost:3306/SoftwarePraktikum_WS1920?user=root&password=student&useUnicode=true&useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static String googleUrl = "";
+	
+	/*
+	 * 
+	 * Stellt die Verbindung zur Datenbank her.
+	 * 
+	 * @return con
+	 * 
+	 * Fazit: DBConnection sollte nicht mittels <code>new</code>
+     * instantiiert werden, sondern stets durch Aufruf dieser statischen
+     * Methode.
+	 */
+
+	
+	public static Connection getConnection() {
+		
+		/*
+		 * Herstellung einer DB Verbindung, wenn bisher keine Verbindung besteht
+		 */
+
+		if (con == null) {
+			try {
+				
+				/*
+				 * Neue Instanz mysl Konnektor (JDBC). Bisher nur localUrl, später durch googleUrl ersetzt.
+				 */
+
+				Class.forName("com.mysql.cj.jdbc.Driver"); // lädt den JDBC Driver Connector für mysql
+				con = DriverManager.getConnection(localUrl);
+				
+				/*
+				 * Die Verbindung zur Datenbank wird in der Variablen con mit
+				 * den dazugehoerigen Informationen gespeichert
+				 */
+				 
+				Statement s = con.createStatement();
+				System.out.println("DB Connection!");
+				
+				/*
+				 * Veranschaulichung, ob die Datenbankverbindung zu stande gekommen ist
+				 */
+
+			} catch (Exception e) {
+				con = null;
+
+				e.printStackTrace(); 
+			}
+			
+		}
+
+		return con;
+	}
+
+}
+
+
+
