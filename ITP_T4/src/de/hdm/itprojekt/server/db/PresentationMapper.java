7e1b48b48314e3e701166f09e3df2c5145b7c279
Dominik Thumm <kinimod1996@web.de>
2019-11-15 23:42:11 +0100

    Instanziierung des PresentationMappers + Ergänzung des Konstruktor

--- ITP_T4/src/de/hdm/itprojekt/server/db/PresentationMapper.java
+++ ITP_T4/src/de/hdm/itprojekt/server/db/PresentationMapper.java
@@ -1,5 +1,33 @@
 package de.hdm.itprojekt.server.db;
 
+/**
+ * Die Klasse PresentationMapper bildet <code>Presentation</code> Objekte auf eine
+ * relationale Datenbank ab. Ebenfalls ist es möglich aus Datenbank-Tupel
+ * Java-Objekte zu erzeugen.
+ * 
+ * Zur Verwaltung der Objekte implementiert die Mapper-Klasse entsprechende
+ * Methoden wie z.B. (insert, delete).
+ */
+
 public class PresentationMapper {
+	
+	/**
+	 * Die Klasse PresentationMapper wird nur einmal instantiiert
+	 * (Singleton-Eigenschaft). Damit diese Eigenschaft erfüllt werden kann, wird
+	 * zunächst eine Variable mit dem Schlüsselwort static und dem Standardwert null
+	 * erzeugt. Sie speichert die Instanz dieser Klasse.
+	 */
+
+	private static PresentationMapper presentationMapper = null;
+
+	/**
+	 * Ein geschützter Konstruktor verhindert das erneute erzeugen von weiteren
+	 * Instanzen dieser Klasse durch <code>new</code>.
+	 */
+
+	protected PresentationMapper() {
+
+	}
+
 
 }
