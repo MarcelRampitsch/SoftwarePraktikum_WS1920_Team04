d3530d0a724013dacd5c2fe9ec19566b9a3a6761
serhat-ulus <serhatulus@gmx.net>
2019-11-16 11:26:54 +0100

    Kommentare ergänzt

--- ITP_T4/src/de/hdm/itprojekt/server/db/CinemaMapper.java
+++ ITP_T4/src/de/hdm/itprojekt/server/db/CinemaMapper.java
@@ -2,18 +2,39 @@ package de.hdm.itprojekt.server.db;
 
 public class CinemaMapper {
 	
+	/**
+	 * Die Klasse CinemaMapper bildet <code>Cinema</code> Objekte auf eine
+	 * relationale Datenbank ab. Ebenfalls ist es möglich aus Datenbank-Tupel
+	 * Java-Objekte zu erzeugen.
+	 * 
+	 * Zur Verwaltung der Objekte implementiert die Mapper-Klasse entsprechende
+	 * Methoden wie z.B. (insert, delete,update..).
+	 */
 	
 	private static CinemaMapper cinemaMapper =null;
 	
-	
-	
-	
-	
+
+	/**
+	 * Die Klasse CinemaMapper wird nur einmal instantiiert
+	 * (Singleton-Eigenschaft). Damit diese Eigenschaft erfüllt werden kann, wird
+	 * zunächst eine Variable mit dem Schlüsselwort static und dem Standardwert null
+	 * erzeugt. Sie speichert die Instanz dieser Klasse.
+	 */
 	
 	protected CinemaMapper() {
 		
 	}
 	
+	
+	/**
+	 * Methode zum Sicherstellen der Singleton-Eigenschaft. Diese sorgt dafür, dass
+	 * nur eine einzige Instanz der CinemaMapper-Klasse existiert. Aufgerufen wird
+	 * die Klasse somit über CinemaMapper.cinemaMapper() und nicht über den
+	 * New-Operator.
+	 * 
+	 * @return Das <code/>CinemaMapper<code/> Objekt.
+	 */
+
 	
 	public static CinemaMapper CinemaMapper() {
 		if (cinemaMapper == null) {
