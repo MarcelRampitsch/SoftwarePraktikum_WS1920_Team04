231fa3d20f7af31bcb5f771279643ec374cac3e9
serhat-ulus <serhatulus@gmx.net>
2019-11-16 10:22:07 +0100

    Klassenvariable hinzugefügt + Methode für die Singleton-Eigenschaft

--- ITP_T4/src/de/hdm/itprojekt/server/db/VoteMapper.java
+++ ITP_T4/src/de/hdm/itprojekt/server/db/VoteMapper.java
@@ -3,13 +3,19 @@ package de.hdm.itprojekt.server.db;
 public class VoteMapper {
 
 	
-	
+	private static VoteMapper voteMapper = null;
 
-	
-	
 	
 	
 	public VoteMapper() {
 		
+	}
+	
+	
+	public static VoteMapper PresentationMapper() {
+		if (voteMapper == null) {
+			voteMapper = new VoteMapper();
+		}
+		return voteMapper;
 	}
 }
