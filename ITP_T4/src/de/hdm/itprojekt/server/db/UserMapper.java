6e8c087a2fdd9d818c68e6ef2cb7ed53271a099c
Van Duy Ho <v.duy.ho@gmail.com>
2019-11-20 14:17:26 +0100

    Methoden für UserMapper hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/server/db/UserMapper.java
+++ ITP_T4/src/de/hdm/itprojekt/server/db/UserMapper.java
@@ -1,5 +1,17 @@
 package de.hdm.itprojekt.server.db;
 
 public class UserMapper {
+	
+	private static UserMapper userMapper =null;
+	
+	protected UserMapper() {
+		
+	}
 
+	public static UserMapper UserMapper() {
+		if (userMapper == null) {
+			userMapper = new UserMapper();
+		}
+		return userMapper;
+	}
 }
