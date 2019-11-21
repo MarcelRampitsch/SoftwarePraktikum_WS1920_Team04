d542b8a9d75400497946fc8c3c2a3189541ce885
Dominik Thumm <kinimod1996@web.de>
2019-11-21 17:17:11 +0100

    EditorForm User Erstellung + BO User erstellung

--- ITP_T4/src/de/hdm/itprojekt/client/gui/EditorForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/EditorForm.java
@@ -1,5 +1,25 @@
 package de.hdm.itprojekt.client.gui;
 
-public class EditorForm {
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojekt.shared.bo.User;
+
+
+/**
+ * 
+ * @author DominikThumm
+ * EditorForm vereint alle grafischen Elemente dieses Packages, zur kompletten Anzeige
+ * des Kino-Umfragen-Verwaltungstools
+ */
+
+public class EditorForm extends VerticalPanel {
+	
+	/* 
+	 * currentUser speichert den aktuellen Nutzer
+	 */
+	protected User currentUser = null;
+	
+	
+	
 
 }
---
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/User.java
@@ -0,0 +1,8 @@
+package de.hdm.itprojekt.shared.bo;
+
+public class User extends BusinessObject {
+	
+	private static final long serialversionUID = 1l;
+
+
+}
