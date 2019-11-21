ca4273f3f990c50125c7f1779256cc03b579b0da
serhat-ulus <serhatulus@gmx.net>
2019-11-21 21:58:30 +0100

    CinemaMapper durch die Insertmethode ergänzt

--- ITP_T4/src/de/hdm/itprojekt/server/db/CinemaGroupMapper.java
+++ ITP_T4/src/de/hdm/itprojekt/server/db/CinemaGroupMapper.java
@@ -1,5 +1,12 @@
 package de.hdm.itprojekt.server.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import de.hdm.itprojekt.shared.bo.CinemaGroup;
+
 public class CinemaGroupMapper {
 	
 	/**
@@ -13,7 +20,7 @@ public class CinemaGroupMapper {
 	
 	private static CinemaGroupMapper cinemaGroupMapper =null;
 	
-
+//
 	/**
 	 * Die Klasse CinemaGroupMapper wird nur einmal instantiiert
 	 * (Singleton-Eigenschaft). Damit diese Eigenschaft erfüllt werden kann, wird
@@ -42,4 +49,43 @@ public class CinemaGroupMapper {
 		}
 		return cinemaGroupMapper;
 	}
+	
+	
+	
+	public CinemaGroup insert(CinemaGroup c) {
+		
+		Connection con = DBConnection.getConnection();
+		
+		try {
+			
+			PreparedStatement insert  = 
+					
+					con.prepareStatement("INSERT INTO softwarepraktikum_ws1920.cinemaGroupname(name, userID) VALUES (?,?);");
+			
+					insert.setString(3, c.getName());
+					insert.setInt(4, c.getUserID());
+					
+					insert.executeUpdate();
+					
+					
+				PreparedStatement getnewCinemaGroup= con
+							.prepareStatement("SELECT *FROM softwarepraktikum_ws1920.cinemaGroup ORDER BY creationDate DESC LIMIT 1;");
+					// Ergebnis-Tupel in Objekt umwandeln
+					ResultSet rs = getnewCinemaGroup.executeQuery();
+					if (rs.next()) {
+
+						return new CinemaGroup(rs.getInt("cinemaGroupID"), rs.getTimestamp("creationDate"), rs.getString("name"), rs.getInt("userID"));
+					}
+					 // Fehlerbehandlung hinzufügen
+				} catch (SQLException e2) {
+				      e2.printStackTrace();
+				      return null;
+
+
+					}
+
+					return null;
+		
+	}
+	
 }
