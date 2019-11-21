890079329d76fcad6fb5a34f1d7ebfa7f2fb236b
Dominik Thumm <kinimod1996@web.de>
2019-11-21 17:28:32 +0100

    Erstellung der Panels für die Ansortierung der Formen

--- ITP_T4/src/de/hdm/itprojekt/client/gui/EditorForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/EditorForm.java
@@ -1,5 +1,6 @@
 package de.hdm.itprojekt.client.gui;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.itprojekt.shared.bo.User;
@@ -19,7 +20,41 @@ public class EditorForm extends VerticalPanel {
 	 */
 	protected User currentUser = null;
 	
+	/**
+	 * <code>header</code>: Oberer Teil des Fensters. Erstreckt sich über die ganze Länge der Anwendung
+	 * <code>main</code>: Zentraler Bestandteil. Umschließt alle anderen Panels, außer <code>header</code>
+	 * <code>center</code>: Mitte der Anwendung. Enhält die <code>voteForm</code>
+	 * <code>east</code>: Rechte Seite der Anwendung. Enhält die Formen <code>umfrageForm</code>
+	 * <code>west</code>: Linke Seite der Anwendung. Enhält die Formen <code>gruppenForm</code>
+	 */
+	
+	HorizontalPanel header = new HorizontalPanel();
+	HorizontalPanel main = new HorizontalPanel();
+
+	VerticalPanel center = new VerticalPanel();
+	VerticalPanel west = new VerticalPanel();
+	VerticalPanel east = new VerticalPanel();
+	
+	/**
+	 * 
+	 * @param currentUser 
+	 * Der aktuelle Nutzer wird der EditorForm übergeben. So können alle anderen Formen diesen bei Bedarf verwenden.
+	 * 
+	 */
+	
+	public EditorForm(User currentUser) {
+		this.currentUser = currentUser;
+	}
 	
 	
+	/*
+	 * onLoad-Methode: Wird ausgeführt, wenn das Panel, dem Browser hinzugefügt wurde. 
+	 * Die dieser Klasse dazugehörigen grafischen Elemente werden dem Panel hinzugefügt.
+	 */
+	
+	public void onLoad() {
+		super.onLoad();
+	
+	}
 
 }
