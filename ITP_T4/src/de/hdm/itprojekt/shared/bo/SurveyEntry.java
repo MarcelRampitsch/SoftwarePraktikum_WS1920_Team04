294c0d53635ce41d4fae2e0899c76e2f4bd2e28a
Van Duy Ho <v.duy.ho@gmail.com>
2019-11-24 23:21:58 +0100

    Add attributes for class SurveyEntry

--- ITP_T4/src/de/hdm/itprojekt/shared/bo/SurveyEntry.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/SurveyEntry.java
@@ -1,5 +1,12 @@
 package de.hdm.itprojekt.shared.bo;
 
-public class SurveyEntry {
+public class SurveyEntry extends BusinessObject {
 
+	private static final long serialversionUID = 1l;
+	
+	private int surveyID;
+	private int presentationID;
+
+	
+	
 }
