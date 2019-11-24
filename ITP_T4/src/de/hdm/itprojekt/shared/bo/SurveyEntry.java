b8260ca2eeb11f8b6c3e0ac7e6f4e489e45f8842
Van Duy Ho <v.duy.ho@gmail.com>
2019-11-24 23:28:51 +0100

    Add getter and setter for class SurveyEntry

--- ITP_T4/src/de/hdm/itprojekt/shared/bo/SurveyEntry.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/SurveyEntry.java
@@ -14,6 +14,20 @@ public class SurveyEntry extends BusinessObject {
 		
 	}
 	
+	public int getSurveyID() {
+		return surveyID;
+	}
 	
+	public void setSurveyID(int surveyID) {
+		this.surveyID = surveyID;
+	}
+	
+	public int getPresentationID() {
+		return presentationID;
+	}
+	
+	public void setPresentationID(int presentationID) {
+		this.presentationID = presentationID;
+	}
 	
 }
