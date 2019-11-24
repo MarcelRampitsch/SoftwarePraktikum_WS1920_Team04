ff5ce5361a0069f665d35401fddaca39f1db2bcd
Van Duy Ho <v.duy.ho@gmail.com>
2019-11-24 23:04:21 +0100

    Add attributes to class Vote

--- ITP_T4/src/de/hdm/itprojekt/shared/bo/Vote.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/Vote.java
@@ -1,5 +1,14 @@
 package de.hdm.itprojekt.shared.bo;
 
-public class Vote {
+public class Vote extends BusinessObject {
 
+	private static final long serialversionUID = 1l;
+	
+	private int surveyentryID;
+	private int userID;
+	
+	
+	
+	
+	
 }
