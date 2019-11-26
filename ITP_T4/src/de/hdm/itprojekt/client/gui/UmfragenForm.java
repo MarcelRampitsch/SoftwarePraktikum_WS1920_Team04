3b09c40a96ac4084a691a94a62bd1e96341b5ff2
Dominik Thumm <kinimod1996@web.de>
2019-11-26 11:48:02 +0100

    UmfragenForm Erstellung und Ergänzung mit Code

--- ITP_T4/src/de/hdm/itprojekt/client/gui/UmfragenForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/UmfragenForm.java
@@ -1,10 +1,71 @@
 package de.hdm.itprojekt.client.gui;
 
-public class UmfragenForm {
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+
+public class UmfragenForm extends VerticalPanel {
 	
 	
 	
+    public UmfragenForm() {
+		
+	}
+
+    Button umfrageErstellung = new Button("Neue Umfrage erstellen:");
+
+    HorizontalPanel umfrageEintragPanel =new HorizontalPanel();
+    
+    
+	FlexTable umfrageEintrag = new FlexTable();  
+
 	
+	public void onLoad() {
+		super.onLoad();
+		
+		
+		
+		//CSS- Stylename Vergabe
+		umfrageErstellung.addStyleName("UmfrageButton");
+		this.addStyleName("UmfragenForm");
+		
+		umfrageEintrag.setStyleName("flexTable");  
+		
+		
+		umfrageEintragPanel.add(umfrageEintrag);
+	    
+		umfrageEintragPanel.add(umfrageErstellung);
+
+		
+		this.add(umfrageErstellung);
+		
+		this.add(umfrageEintragPanel);
+		
+		umfrageErstellung.addClickHandler(new OpenUpClickHandler());
+
+
+		
+		
+		
+		
+	}
+	
+	
+       class OpenUpClickHandler implements ClickHandler{
+		
+		 public void onClick(ClickEvent event) {
+			
+			UmfragenDeleteForm uf = new UmfragenDeleteForm();
+			uf.umfrageEditForm();
+		}
+	}
+		
+
 	
 
 }
