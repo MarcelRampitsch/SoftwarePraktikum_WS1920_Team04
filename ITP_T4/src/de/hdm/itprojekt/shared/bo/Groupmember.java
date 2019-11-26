45ccfdf0fb118030d3bf8b35d6fbf1097c0e4498
BastianGroezinger <bg036@hdm-stuttgart.de>
2019-11-26 10:24:55 +0100

    Groupmember Business Object und Mapper erstellt

---
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/Groupmember.java
@@ -0,0 +1,36 @@
+package de.hdm.itprojekt.shared.bo;
+
+import java.sql.Timestamp;
+
+public class Groupmember extends BusinessObject{
+
+		private static final long serialversionUID = 1l;
+			
+		//Fremdschlüsselattribute
+		private int userID;
+		private int groupID;
+		
+		//Konstruktor, der zum tragen kommt, falls eine Instanz dieser Klasse erstellt wird
+		public Groupmember(int id, Timestamp creationDate, int groupID, int userID) {
+			super(id, creationDate);
+			this.groupID=groupID;
+			this.userID=userID;
+		}
+
+		public int getUserID() {
+			return userID;
+		}
+
+		public void setUserID(int userID) {
+			this.userID = userID;
+		}
+
+		public int getGroupID() {
+			return groupID;
+		}
+
+		public void setGroupID(int groupID) {
+			this.groupID = groupID;
+		}
+		
+}
