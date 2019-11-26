b57099f064b953430c9b41cd1c1878a38654bd92
Dominik Thumm <kinimod1996@web.de>
2019-11-26 21:47:35 +0100

    Erstellung der zweiten EntryPoint Klasse Admin

---
+++ ITP_T4/src/de/hdm/itprojekt/client/Admin.java
@@ -0,0 +1,17 @@
+package de.hdm.itprojekt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+
+public class Admin implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		
+	//	RootPanel.get().add(new AdminForm(null));
+
+		
+	} 
+
+}
