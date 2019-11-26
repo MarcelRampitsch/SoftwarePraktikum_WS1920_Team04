2b5c5146e7963f5a9af690d92229279001733f0d
Dominik Thumm <kinimod1996@web.de>
2019-11-26 20:43:46 +0100

    Erstellung des gui.admin package + Klassenerstellungen

---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/VerwaltungsForm.java
@@ -0,0 +1,61 @@
+package de.hdm.itprojekt.client.gui.admin;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+
+public class VerwaltungsForm extends VerticalPanel {
+	
+	
+	Button sp1 = new Button("Spielplan verwalten");
+	
+	HorizontalPanel test = new HorizontalPanel();
+
+
+	
+	VerwaltungsForm(){
+		
+	}
+	
+	//VerwaltungForm(Administrator a){
+	//	this.adminUser=a;
+//	}
+	
+	public void onLoad() {
+		
+		sp1.addStyleName("verwaltung");
+		super.onLoad();
+		
+		test.add(sp1);
+		
+		this.add(test);
+		
+		sp1.addClickHandler(new OpenVUpClickHandler());
+
+		
+	}
+	
+	 class OpenVUpClickHandler implements ClickHandler {
+	    	
+		    
+	    	
+			public void onClick(ClickEvent event) {
+				
+			//	greetingService.greetServer(schmeckt.getText(), new AsyncCallback<String>() {
+
+				
+				VerwaltungsOpenForm gr = new VerwaltungsOpenForm();
+			//	gr.openVerwaltungEditForm();
+				// Anpassen!!
+
+	    }}
+	
+	
+	
+	
+
+}
---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/VerwaltungsOpenForm.java
@@ -0,0 +1,10 @@
+package de.hdm.itprojekt.client.gui.admin;
+
+import com.google.gwt.user.client.ui.DialogBox;
+
+public class VerwaltungsOpenForm extends DialogBox {
+	
+	
+	// hier bitte Code aus "SpielPlanFormAdmin" admin ergänzen
+
+}
