c950d9de2fc15ea80b08d51b6ad3a37771166b15
serhat-ulus <serhatulus@gmx.net>
2019-11-27 15:23:29 +0100

    Klasse CellListForm erstellt, das das Anzeigen der Gruppen realisiert

---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/CellListForm.java
@@ -0,0 +1,65 @@
+package de.hdm.itprojekt.client.gui;
+
+
+/**
+ * 
+ * @author serhatulus
+ *
+ */	
+
+import java.util.Arrays;
+import java.util.List;
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.SelectionChangeEvent;
+import com.google.gwt.view.client.SingleSelectionModel;
+
+public class CellListForm extends VerticalPanel {
+	
+	
+	 private static final List<String> Gruppen = Arrays.asList("Gruppe1", "Gruppe2",
+		      "Gruppe3", "Gruppe4", "Gruppe5", "Gruppe6", "Gruppe8"); 
+   
+   
+   
+   public CellListForm() {
+
+}
+   
+   public void onLoad() {
+	   super.onLoad();
+	   
+	   //INSTANZ DER KLASSE TEXTCELL
+	   TextCell textCell = new TextCell();
+	   
+	   //INSTANZ DER KKASSE CELLLIST PLUS DIE VORHER ERSTELLTE INSTANT VON TEXTCELL HIER ÜBERGEBEN 
+	   CellList<String> cellList = new CellList<String>(textCell);
+	   
+	   cellList.addStyleName("gruppenliste");
+	    cellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+	    final SingleSelectionModel<String> selectionModel = new SingleSelectionModel<String>();
+	    cellList.setSelectionModel(selectionModel);
+	    selectionModel.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+	      public void onSelectionChange(SelectionChangeEvent event) {
+	        String selected = selectionModel.getSelectedObject();
+	        if (selected != null) {
+	          Window.alert("You selected : " + selected);
+	          
+	        }
+	      }
+	    });
+	   
+	    
+	    cellList.setRowCount(Gruppen.size(), true);
+	    cellList.setRowData(0, Gruppen);
+	   
+	    this.add(cellList);
+	 
+   }
+   
+ 
+
+}
