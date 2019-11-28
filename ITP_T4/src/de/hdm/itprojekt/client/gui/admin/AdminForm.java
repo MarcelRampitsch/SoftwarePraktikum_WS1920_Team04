ed82e37b9a0e9bae9f2206f3e97ff5d4251e34a8
Van Duy Ho <v.duy.ho@gmail.com>
2019-11-28 14:49:47 +0100

    Update AdminForm

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/AdminForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/AdminForm.java
@@ -1,39 +1,41 @@
-package de.hdm.itprojekt.client.gui.admin;
+package de.hdm.client.gui.admin;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-
-
+import de.hdm.shared.bo.Administrator;
 
 /**
  * 
- * @author DominikThumm
+ * @author DominikThumm, VanDuyHo
  * AdminForm vereint alle grafischen Elemente dieses Packages, zur kompletten Anzeige
  * des Admin-Client
+ * 
  */
 
-public class AdminForm extends VerticalPanel {
+public class AdminForm extends VerticalPanel{
 	
 	/* 
 	 * currentUser speichert den aktuellen Nutzer
 	 */
-	//protected Administrator adminUser = null;
+	protected Administrator adminUser = null;
 	
 	/**
-	 * <code>header</code>: Oberer Teil des Fensters. Erstreckt sich über die ganze Länge der Anwendung
-	 * <code>main</code>: Zentraler Bestandteil. Umschließt alle anderen Panels, außer <code>header</code>
-	 * <code>west</code>: Linke Seite der Anwendung. Enhält die Formen <code>VerwaltungsForm</code>
+	 * 
+	 * <code>main</code>: Zentraler Bestandteil. Umschließt alle anderen Panels
 	 */
 	
-	HorizontalPanel header = new HorizontalPanel();
-	HorizontalPanel main = new HorizontalPanel();
+	VerticalPanel main = new VerticalPanel();
 	
-	VerticalPanel west = new VerticalPanel();
+	Button cinemaButton = new Button("Cinema");
+	Button movieButton = new Button("Movie");
+	Button timeslotButton = new Button("Timeslot");
+	Button presentationButton = new Button("Presentation");
+
 	
 	/**
 	 * 
@@ -41,55 +43,51 @@ public class AdminForm extends VerticalPanel {
 	 * Der aktuelle Nutzer (Admin) wird der AdminForm übergeben. So können alle anderen Formen diesen bei Bedarf verwenden.
 	 * 
 	 */
+	public AdminForm(Administrator adminUser) {
+		this.adminUser = adminUser;
+	}
 	
-	//public AdminForm(Administrator adminUser) {
-	//	this.adminUser = adminUser;
-	//}
 	
 	/*
 	 * onLoad-Methode: Wird ausgeführt, wenn das Panel, dem Browser hinzugefügt wurde. 
 	 * Die dieser Klasse dazugehörigen grafischen Elemente werden dem Panel hinzugefügt.
 	 */
 	public void onLoad() {
-		super.onLoad();
 		
+		super.onLoad();
+
 		/*
 		 * CSS-StyleName-Vergabe, um Panels direkt anzusprechen.
 		 */
-	//	Button b1 = new Button("sdv");
 
 		this.addStyleName("AdminForm");
-		header.addStyleName("AdminHeader");
+		
 		main.addStyleName("AdminMain");
-		west.addStyleName("AdminWest");
 		
-		/**
-		 *  Jede Form wird demjeweiligen Panel hinzugefügt.
-		 */
-		
-		VerwaltungsForm verwaltungsForm = new VerwaltungsForm();
+		Image logo = new Image("Offical_Logo.png");
+		VerwaltungForm verwaltungsForm = new VerwaltungForm(adminUser);
+	
+		main.add(cinemaButton);
+		main.add(movieButton);
+		main.add(timeslotButton);
+		main.add(presentationButton);
 
-		
-		west.add(verwaltungsForm);
-		
-	//	header.add(logo);
-		
-		//Button, dessen ClickEvent zum Admin Mode führt.
-		Button toBesucher = new Button("Kinobesucher", new ClickHandler() {
-			@Override
-			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
-				Window.Location.replace("/ITP_T4.html");
-			}
-		});
-		west.add(toBesucher);
-		
-		//Die zwei Hauptpanels werden der AdminForm hinzugefügt.
-		this.add(header);
 		this.add(main);
-		this.add(west);
+	
+		RootPanel.get("container").add(main);
+
+	}
+
+	private class CinemaHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+
+			
+		}
+		
 	}
 	
 	
-
 }
