2291bfec4862e2e7c9da2a734e77d7676a027758
serhat-ulus <serhatulus@gmx.net>
2019-11-29 00:37:56 +0100

    Klasse TimeSlotForm ergänzt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/TimeslotForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/TimeslotForm.java
@@ -1,5 +1,60 @@
 package de.hdm.itprojekt.client.gui.admin;
 
-public class TimeslotForm {
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+
+/**
+ * 
+ * @author SerhatUlus
+ *
+ */
+public class TimeslotForm extends VerticalPanel {
+	
+	
+	
+	
+	TextBox timeslotbox = new TextBox();
+    Label timeslotLabel = new Label("Timeslot");
+    
+    Button timeslotEdit = new Button("Edit");
+    Button timeslotNew = new Button("New");
+    Button timeslotDelete = new Button("Delete");
+    
+    Button timeslotAdd = new Button("add");
+    
+    HorizontalPanel timeslotaddbox = new HorizontalPanel();
+    HorizontalPanel buttonbox = new HorizontalPanel();
+
+    
+    public TimeslotForm() {
+    	
+    }
+
+    
+    public void onLoad() {
+    	
+    	super.onLoad();
+    	
+    	this.add(timeslotLabel);
+    	
+    	timeslotaddbox.add(timeslotbox);
+    	timeslotaddbox.add(timeslotAdd);
+    	
+    	buttonbox.add(timeslotEdit);
+    	buttonbox.add(timeslotNew);
+    	buttonbox.add(timeslotDelete);
+    	
+    	this.add(timeslotaddbox);
+    	this.add(buttonbox);
+     	
+    	
+    	
+    }
 
 }
+
+
