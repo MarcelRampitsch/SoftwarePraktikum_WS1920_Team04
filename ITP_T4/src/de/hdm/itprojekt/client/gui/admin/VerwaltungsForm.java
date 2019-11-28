4a6e77ecb4e4a8ddba8885ae06adb57c7fae6183
Dominik Thumm <kinimod1996@web.de>
2019-11-28 21:48:40 +0100

    Erstellung sämtlicher Widgets zur anzeige das Admin Client

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/VerwaltungsForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/VerwaltungsForm.java
@@ -4,16 +4,89 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.StackPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 
 
 public class VerwaltungsForm extends VerticalPanel {
 	
 	
-	Button sp1 = new Button("Spielplan verwalten");
+//	Button sp1 = new Button("Spielplan verwalten");
 	
-	HorizontalPanel test = new HorizontalPanel();
+//	HorizontalPanel test = new HorizontalPanel();
+	
+	
+	// Cinema/ CinemaGroup, Movie, Timeslot und Präsentation werden einem StackPanel hinzugefügt
+    StackPanel panel = new StackPanel();
+ 
+    //Cinema/ CinemaGroup
+    TextBox cinemaGroupBox = new TextBox();
+    TextBox cinemaBox = new TextBox();
+    
+    Label cinemaGroupLabel = new Label("CinemaGroup");
+    Label cinemaLabel = new Label("Cinema");
+    
+    Button cinemaGroupEdit = new Button("Edit");
+    Button cinemaGroupNew = new Button("New");
+    Button cinemaGroupDelete = new Button("Delete");
+    
+    Button cinemaEdit = new Button("Edit");
+    Button cinemaNew = new Button("New");
+    Button cinemaDelete = new Button("Delete");
+    
+    Button cinemaGroupAdd = new Button("add");
+    Button cinemaAdd = new Button("add");
+    
+    
+    //Movie
+    TextBox moviebox = new TextBox();
+    Label movieLabel = new Label("Movie");
+    
+    Button movieEdit = new Button("Edit");
+    Button movieNew = new Button("New");
+    Button movieDelete = new Button("Delete");
+    
+    Button movieAdd = new Button("add");
+    
+    //Timeslot
+    TextBox timeslotbox = new TextBox();
+    Label timeslotLabel = new Label("Movie");
+    
+    Button timeslotEdit = new Button("Edit");
+    Button timeslotNew = new Button("New");
+    Button timeslotDelete = new Button("Delete");
+    
+    Button timeslotAdd = new Button("add");
+    
+    //Presentation
+    Label pcinemaLabel = new Label("Cinema");
+    Label pmovieLabel = new Label("Movie");
+    Label ptimeslotLabel = new Label("Timeslot");
+
+    ListBox cinemaDrop = new ListBox();
+    ListBox movieDrop = new ListBox();
+    ListBox timeslotDrop = new ListBox();
+    
+    Label dateLabel = new Label("Date");
+    
+    DatePicker datePicker = new DatePicker();
+    
+    Button search = new Button("Search");
+
+    TextBox presentationbox = new TextBox();
+    Label presentationLabel = new Label("Movie");
+    
+    Button presentationEdit = new Button("Edit");
+    Button presentationNew = new Button("New");
+    Button presentationDelete = new Button("Delete");
+    
+    Button presentationAdd = new Button("add");
+
 
 
 	
@@ -26,18 +99,24 @@ public class VerwaltungsForm extends VerticalPanel {
 //	}
 	
 	public void onLoad() {
-		
-		sp1.addStyleName("verwaltung");
 		super.onLoad();
+
+//		sp1.addStyleName("verwaltung");
 		
-		test.add(sp1);
+//		test.add(sp1);
 		
-		this.add(test);
+//		this.add(test);
 		
-		sp1.addClickHandler(new OpenVUpClickHandler());
+//		sp1.addClickHandler(new OpenVUpClickHandler());
 
 		
 	}
+	
+	
+	
+	
+	
+	
 	
 	 class OpenVUpClickHandler implements ClickHandler {
 	    	
