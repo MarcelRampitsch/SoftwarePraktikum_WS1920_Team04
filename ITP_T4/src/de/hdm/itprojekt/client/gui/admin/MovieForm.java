5b5b55ba55106c689920f513615db6e7da69f692
serhat-ulus <serhatulus@gmx.net>
2019-11-29 00:37:30 +0100

    Klasse MovieForm ergänzt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/MovieForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/MovieForm.java
@@ -1,5 +1,56 @@
 package de.hdm.itprojekt.client.gui.admin;
 
-public class MovieForm {
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * 
+ * @author SerhatUlus
+ *
+ */
+
+public class MovieForm  extends VerticalPanel{
+	
+	
+	TextBox moviebox = new TextBox();
+    Label movieLabel = new Label("Movie");
+    
+    Button movieEdit = new Button("Edit");
+    Button movieNew = new Button("New");
+    Button movieDelete = new Button("Delete");
+    
+    Button movieAdd = new Button("add");
+    
+    HorizontalPanel movieaddbox = new HorizontalPanel();
+    HorizontalPanel buttonbox = new HorizontalPanel();
+
+    
+    public MovieForm() {
+    	
+    }
+
+    
+    public void onLoad() {
+    	
+    	super.onLoad();
+    	
+    	this.add(movieLabel);
+    	
+    	movieaddbox.add(moviebox);
+    	movieaddbox.add(movieAdd);
+    	
+    	buttonbox.add(movieEdit);
+    	buttonbox.add(movieNew);
+    	buttonbox.add(movieDelete);
+    	
+    	this.add(movieaddbox);
+    	this.add(buttonbox);
+     	
+    	
+    	
+    }
 
 }
