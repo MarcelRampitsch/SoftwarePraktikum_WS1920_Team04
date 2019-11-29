5b9752579727292007fb04c18136ad09f2a435c3
Dominik Thumm <kinimod1996@web.de>
2019-11-29 22:12:29 +0100

    Grundstruktur des ClickHandlers/ Callback zum löschen eines Movie

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/MovieForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/MovieForm.java
@@ -1,10 +1,16 @@
 package de.hdm.itprojekt.client.gui.admin;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojekt.shared.bo.CinemaGroup;
+import de.hdm.itprojekt.shared.bo.Movie;
 
 /**
  * 
@@ -49,8 +55,37 @@ public class MovieForm  extends VerticalPanel{
     	this.add(movieaddbox);
     	this.add(buttonbox);
      	
-    	
-    	
     }
+    
+    /*
+	 * Ab hier folgen alle CLICKHANDLER und CALLBACKS dieser Klasse!
+	 */
+	
+	/**
+	 * ClickHandler für Erstellung eines Movie
+	 */
 
+    public class addMovieClickHandler implements ClickHandler{
+		
+		public void onClick(ClickEvent event) {
+			
+		}
+		
+	}
+	
+	private class addCinemaGroupCallback implements AsyncCallback <Movie>{
+
+		@Override
+		public void onFailure(Throwable caught) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSuccess(Movie result) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
