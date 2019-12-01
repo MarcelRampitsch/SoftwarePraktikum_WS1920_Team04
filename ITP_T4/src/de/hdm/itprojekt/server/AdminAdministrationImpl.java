284ac56b97714e9ac311a38197f7fde540c22be1
MarcelRampitsch <mr164@hdm-stuttgart.de>
2019-12-02 00:51:59 +0100

    Hinzufügen Mapper Initialisierung AdminAdministrationImpl

--- ITP_T4/src/de/hdm/itprojekt/server/AdminAdministrationImpl.java
+++ ITP_T4/src/de/hdm/itprojekt/server/AdminAdministrationImpl.java
@@ -4,12 +4,46 @@ import de.hdm.itprojekt.shared.AdminAdministration;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.itprojekt.server.db.DBConnection;
+import de.hdm.itprojekt.server.db.CinemaGroupMapper;
+import de.hdm.itprojekt.server.db.CinemaMapper;
+import de.hdm.itprojekt.server.db.GroupMapper;
+import de.hdm.itprojekt.server.db.GroupmemberMapper;
+import de.hdm.itprojekt.server.db.MovieMapper;
+import de.hdm.itprojekt.server.db.PresentationMapper;
+import de.hdm.itprojekt.server.db.SurveyEntryMapper;
+import de.hdm.itprojekt.server.db.SurveyMapper;
+import de.hdm.itprojekt.server.db.TimeslotMapper;
+import de.hdm.itprojekt.server.db.UserMapper;
+import de.hdm.itprojekt.server.db.VoteMapper;
+
+
 /**
  * The server-side implementation of the RPC service.
  */
 
 public class AdminAdministrationImpl extends RemoteServiceServlet implements AdminAdministration {
+	
+	private CinemaGroupMapper cgMapper = null;
+	
+	private CinemaMapper cMapper = null;
+	
+	private MovieMapper mMapper = null;
+	
+	private PresentationMapper pMapper = null;
+	
+	private TimeslotMapper tMapper = null;
 
-	public void init() {
+
+	public AdminAdministrationImpl() throws IllegalArgumentException{
+		
+	}
+	public void init() throws IllegalArgumentException {
+		
+		this.cgMapper = CinemaGroupMapper.CinemaGroupMapper();
+		this.cMapper = CinemaMapper.CinemaMapper();
+		this.mMapper = MovieMapper.MovieMapper();
+		this.pMapper = PresentationMapper.PresentationMapper();
+		this.tMapper = TimeslotMapper.TimeslotMapper();
 	}
 }
