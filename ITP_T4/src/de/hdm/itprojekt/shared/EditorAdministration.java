9fa28cf79f7e9007503f099f6011b8760eaaac18
MarcelRampitsch <mr164@hdm-stuttgart.de>
2019-12-02 17:04:49 +0100

    Ergänzungen EditorAdministration create, getter & setter

--- ITP_T4/src/de/hdm/itprojekt/shared/EditorAdministration.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/EditorAdministration.java
@@ -4,7 +4,42 @@ import com.google.gwt.user.client.rpc.RemoteService;
 
 public interface EditorAdministration extends RemoteService {
 	
-	void init();
-
-
+	void init() throws IllegalArgumentException;
+	
+	public Survey createSurvey(String name)
+			throws IllegalArgumentException;
+	
+	public SurveyEntry createSurveyEntry()
+		throws IllegalArgumentException;
+	
+	public User createUser(String nickname, String email)
+		throws IllegalArgumentException;
+	
+	public Group createGroup(String name)
+		throws IllegalArgumentException;
+	
+	public GroupMember createGroupMember()
+		throws IllegalArgumentException;
+	
+	public Survey getSurvey() throws IllegalArgumentException;
+	
+	public void setSurvey(Survey s) throws IllegalArgumentException;
+	
+	public SurveyEntry getSurveyEntry() throws IllegalArgumentException;
+	
+	public void setSurveyEntry(SurveyEntry se) throws IllegalArgumentException;
+	
+	public User getUser() throws IllegalArgumentException;
+	
+	public void setUser(User u) throws IllegalArgumentException;
+	
+	public Group getGroup() throws IllegalArgumentException;
+	
+	public void setGroup(Group g) throws IllegalArgumentException;
+	
+	public GroupMember getGroupMember() throws IllegalArgumentException;
+	
+	public void setGroupMember(GroupMember gm) throws IllegalArgumentException;
+	
+	}
 }
