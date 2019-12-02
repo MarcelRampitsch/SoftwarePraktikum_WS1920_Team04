74cab187de6ed7046b372ff632fcdf1bf5601cde
serhat-ulus <serhatulus@gmx.net>
2019-12-02 16:27:14 +0100

    Klasse erstellt, die das Anzeigen der DialogBox mitsamt dem Content
    realisiert

---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/TimeSlotDialogBox.java
@@ -0,0 +1,93 @@
+package de.hdm.itprojekt.client.gui.admin;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class TimeSlotDialogBox extends DialogBox {
+	
+	HorizontalPanel timeslotcontent = new HorizontalPanel();
+	
+	VerticalPanel content = new VerticalPanel();
+	
+	Label timeslot = new Label("Cinemagroup:");
+	
+	TextBox timeslotbox = new TextBox();
+	
+	Button close = new Button("X");
+	Button safe = new Button("save");
+	
+	
+	
+	public TimeSlotDialogBox () {
+		
+		
+	}
+	
+	public void onLoad() {
+		
+		super.onLoad();
+		
+		content.add(close);
+		close.addClickHandler(new closehandler());
+		
+		content.add(timeslot);
+		content.add(timeslotbox);
+		
+		content.add(timeslotcontent);
+		
+		content.add(safe);
+		safe.addClickHandler(new safehandler());
+		
+		this.add(content);
+		
+	}
+	
+	
+	
+	
+	
+	public void closetimeslot() {
+		this.hide();
+		this.clear();
+		this.removeFromParent();
+		this.setAnimationEnabled(false);
+		this.setGlassEnabled(false);
+	
+
+}
+	
+	public void opentimeslot() {
+			this.setGlassEnabled(true);
+			this.setAnimationEnabled(true);
+			this.center();
+			this.show();
+	}
+	
+	
+	private class closehandler implements ClickHandler {
+		
+		public void onClick(ClickEvent event) {
+			closetimeslot();
+		}
+		
+		
+	}
+	
+	private class safehandler implements ClickHandler{
+		
+		public void onClick(ClickEvent event) {
+			Window.alert("EINGABE GESICHERT");
+		}
+	}
+	
+	
+	
+
+}
