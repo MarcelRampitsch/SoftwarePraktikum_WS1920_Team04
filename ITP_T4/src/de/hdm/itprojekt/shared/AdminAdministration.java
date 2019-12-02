8fb34c459420f9438141c45341c89df12349d8bb
Dominik Thumm <kinimod1996@web.de>
2019-12-02 12:57:26 +0100

    Hinzufügen der addCinema Methode

--- ITP_T4/src/de/hdm/itprojekt/shared/AdminAdministration.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/AdminAdministration.java
@@ -2,8 +2,12 @@ package de.hdm.itprojekt.shared;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 
+import de.hdm.itprojekt.shared.bo.Cinema;
+
 public interface AdminAdministration extends RemoteService {
 
 	void init();
+	
+	Cinema addCinema(Cinema c) throws IllegalArgumentException;
 
 }
