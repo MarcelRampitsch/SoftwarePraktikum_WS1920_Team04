7fe4ac76414078d68d127b9a4683aa9adee88b69
serhat-ulus <serhatulus@gmx.net>
2019-12-03 20:46:40 +0100

    Anpassung des deleteTimeslotClickHandler durchgeführt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/TimeslotForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/TimeslotForm.java
@@ -1,4 +1,4 @@
-package de.hdm.itprojekt.client.gui.admin;
+	package de.hdm.itprojekt.client.gui.admin;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
