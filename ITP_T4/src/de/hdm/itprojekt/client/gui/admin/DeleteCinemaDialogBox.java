44e5c4cde16633f105e739ab123e3111fc9aebf4
Dominik Thumm <kinimod1996@web.de>
2019-12-04 22:36:35 +0100

    ClickHandler und Callback für delete Cinama erstellt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/DeleteCinemaDialogBox.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/DeleteCinemaDialogBox.java
@@ -2,11 +2,17 @@ package de.hdm.itprojekt.client.gui.admin;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojekt.client.ClientSideSettings;
+import de.hdm.itprojekt.shared.AdminAdministrationAsync;
+import de.hdm.itprojekt.shared.bo.Cinema;
 
 public class DeleteCinemaDialogBox extends DialogBox {
 	
@@ -14,8 +20,9 @@ public class DeleteCinemaDialogBox extends DialogBox {
 	
 	HorizontalPanel horzcontent = new HorizontalPanel();
 
-	
-	
+	AdminAdministrationAsync adminAdministration = ClientSideSettings.getAdminAdministration();
+
+	Cinema cinema = null;
 	
 	Label cinemaLabel = new Label("Cinema wirklich löschen?");
 	
@@ -35,12 +42,17 @@ public class DeleteCinemaDialogBox extends DialogBox {
 		horzcontent.add(no);
 		content.add(horzcontent);
 		no.addClickHandler(new closeCinemaForm());
+		yes.addClickHandler(new deleteCinema());
 		
 		this.add(content);
 		
 		
 		
 	}
+	
+	/*
+	 * Methoden zum Öffnen und Schließen der DeleteCinemaDialogBox.
+	 */
 	
 	public void openCimemaDelete() {
 		this.setGlassEnabled(true);
@@ -58,6 +70,16 @@ public class DeleteCinemaDialogBox extends DialogBox {
 		this.setGlassEnabled(false);
 	}
 	
+	/*
+	 * Ab hier folgen alle CLICKHANDLER und CALLBACKS dieser Klasse!
+	 */
+	
+	
+	
+	/**
+	 * closeCinemaForm ClickHandler: Wird beim Click auf <code> no </code> Button ausgelöst.
+	 * Der User bestätigt damit, dass das Cinema nicht gelöscht werden soll.
+	 */
 	
     private class closeCinemaForm implements ClickHandler{
 		
@@ -69,18 +91,49 @@ public class DeleteCinemaDialogBox extends DialogBox {
 
 	}
     
- private class deleteCinema implements ClickHandler{
+    /**
+	 * deleteCinema ClickHandler: Wird beim Click auf <code> yes </code> Button ausgelöst.
+	 * Der User bestätigt damit, dass das Cinema gelöscht werden soll.
+	 */
+    
+    private class deleteCinema implements ClickHandler{
 		
 		@Override
 		public void onClick(ClickEvent event) {
 			
+			Window.alert("Wir sind im ClickHandler");
+
+			adminAdministration.deleteCinema(cinema.getId(), new DeleteCallBack());
+
+
 		}
 
 	}
     
+    /**
+	 * CallBack des Clickhandlers <code> deleteCinema ClickHandler </code>
+	 * Bei erfolgreichem Rückruf (onSucess) wird das Cinema Objekt gelöscht. Danach wird die dazugehörige <code> DeleteCinemaDialogBox </code> geschlossen. 
+	 * 
+	 */
+	class DeleteCallBack implements AsyncCallback<Void>{
+
+		@Override
+		public void onFailure(Throwable caught) {
+			Window.alert("Fehler");
+			
+		}
+
+		@Override
+		public void onSuccess(Void result) {
+			Window.alert("Erfolg");
+
+			closeCinemaForm();
+
+			
+		}
     
 
     
-	
+	}
 
 }
