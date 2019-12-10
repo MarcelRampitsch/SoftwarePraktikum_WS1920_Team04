03cd2c1dd70808ec9d02b4928757cfe999a39d7f
MarcelRampitsch <mr164@hdm-stuttgart.de>
2019-12-10 02:08:19 +0100

    Referenzen auf Mapper und Import der Packages

--- ITP_T4/src/de/hdm/itprojekt/server/EditorAdministrationImpl.java
+++ ITP_T4/src/de/hdm/itprojekt/server/EditorAdministrationImpl.java
@@ -1,6 +1,7 @@
 package de.hdm.itprojekt.server;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.google.gwt.user.client.Window;
 
 import de.hdm.itprojekt.shared.EditorAdministration;
 import de.hdm.itprojekt.shared.bo.Group;
@@ -11,9 +12,41 @@ import de.hdm.itprojekt.shared.bo.SurveyEntry;
 import de.hdm.itprojekt.shared.bo.User;
 import de.hdm.itprojekt.shared.bo.Vote;
 
+import java.sql.SQLException;
+import java.util.Vector;
+
+import de.hdm.itprojekt.server.db.DBConnection;
+import de.hdm.itprojekt.server.db.GroupMapper;
+import de.hdm.itprojekt.server.db.GroupmemberMapper;
+import de.hdm.itprojekt.server.db.PresentationMapper;
+import de.hdm.itprojekt.server.db.SurveyEntryMapper;
+import de.hdm.itprojekt.server.db.SurveyMapper;
+import de.hdm.itprojekt.server.db.UserMapper;
+import de.hdm.itprojekt.server.db.VoteMapper;
+
 public class EditorAdministrationImpl extends RemoteServiceServlet implements EditorAdministration {
 	
-	public void init() {	
+	private GroupMapper gMapper = null;
+	private GroupmemberMapper gmMapper = null;
+	private PresentationMapper pMapper = null;
+	private SurveyEntryMapper seMapper = null;
+	private SurveyMapper sMapper = null;
+	private UserMapper uMapper = null;
+	private VoteMapper vMapper = null;
+	
+	
+	public EditorAdministrationImpl() throws IllegalArgumentException {
+	}
+	
+	public void init() throws IllegalArgumentException{	
+
+		this.gMapper = GroupMapper.GroupMapper();
+		this.gmMapper = GroupmemberMapper.GroupmemberMapper();
+		this.pMapper = PresentationMapper.PresentationMapper();
+		this.seMapper = SurveyEntryMapper.SurveyEntryMapper();
+		this.sMapper = SurveyMapper.SurveyMapper();
+		this.uMapper = UserMapper.UserMapper();
+		this.vMapper = VoteMapper.VoteMapper();
 	}
 
 	@Override
@@ -177,5 +210,4 @@ public class EditorAdministrationImpl extends RemoteServiceServlet implements Ed
 		// TODO Auto-generated method stub
 		
 	}
-
 }
