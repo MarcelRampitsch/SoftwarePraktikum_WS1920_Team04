9c143885b61b9bcbd29e241f6b5a857e5320b65f
BastianGroezinger <bg036@hdm-stuttgart.de>
2019-12-29 16:20:28 +0100

    ein paar änderungen

---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/PresentationDeleteDialogBox.java
@@ -0,0 +1,127 @@
+package de.hdm.itprojekt.client.gui.admin;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojekt.client.ClientSideSettings;
+import de.hdm.itprojekt.shared.AdminAdministrationAsync;
+import de.hdm.itprojekt.shared.bo.Presentation;
+import de.hdm.itprojekt.shared.bo.User;
+
+/*
+ * 
+ * @author SerhatUlus
+ * Klasse, die das Anzeigen einer DialogBox mitsamt ihrem inhalt realisiert
+ *
+ */
+public class PresentationDeleteDialogBox extends DialogBox {
+
+	VerticalPanel content = new VerticalPanel();
+	
+	HorizontalPanel horzcontent = new HorizontalPanel();
+
+	Presentation presentation = null;
+	
+	Label pres = new Label("Präsentation wirklich löschen?");
+	
+	Button yes = new Button ("yes");
+	Button no = new Button ("no");
+	
+	
+	
+	User user = null;
+	
+	AdminAdministrationAsync adminAdministration = ClientSideSettings.getAdminAdministration();
+
+	
+	public PresentationDeleteDialogBox(Presentation presentation ,User user) {
+		this.presentation=presentation;
+		this.user=user;
+	}
+	
+	
+	public void onLoad() {
+		super.onLoad();
+		
+		
+		content.add(pres);
+		
+		horzcontent.add(yes);
+		horzcontent.add(no);
+		content.add(horzcontent);
+
+		no.addClickHandler(new closePresentation());
+		
+		yes.addClickHandler(new deleteTimeslot());
+		
+		this.add(content);
+				
+	}
+	
+	
+	
+	/*
+	 * Methode die das Anzeigen der DialogBox realisiert
+	 */
+	public void openPresentation() {
+		this.setGlassEnabled(true);
+		this.setAnimationEnabled(true);
+		this.center();
+		this.show();	
+	}
+	
+	/*
+	 * Methode die das schließen des Fensters realisiert
+	 */
+	public void closepresentation() {
+		this.hide();
+		this.clear();
+		this.removeFromParent();
+		this.setAnimationEnabled(false);
+		this.setGlassEnabled(false);
+	}
+	
+	/*
+	 * Der ClickHandler der zum Tragen kommt, falls auf den no-button geklickt wird
+	 */
+    private class closePresentation implements ClickHandler{
+		
+		@Override
+		public void onClick(ClickEvent event) {
+			closepresentation();
+			
+		}
+    }
+    
+    private class deleteTimeslot implements ClickHandler{
+
+	public void onClick(ClickEvent event) {
+		
+
+		adminAdministration.deletePresentation(presentation, new AsyncCallback<Void>() {
+			
+			@Override
+			public void onFailure(Throwable caught) {
+					Window.alert("????");
+			}
+			
+			public void onSuccess(Void result) {
+				closepresentation();
+				RootPanel.get().clear();
+				AdminForm adminform = new AdminForm(user,4);
+				RootPanel.get().add(adminform);
+			}
+		});
+
+	}
+
+}
+}
---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/PresentationNewDialogBox.java
@@ -0,0 +1,191 @@
+package de.hdm.itprojekt.client.gui.admin;
+
+import java.sql.Date;
+import java.util.Vector;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.datepicker.client.DatePicker;
+
+import de.hdm.itprojekt.client.ClientSideSettings;
+import de.hdm.itprojekt.shared.AdminAdministrationAsync;
+import de.hdm.itprojekt.shared.bo.Cinema;
+import de.hdm.itprojekt.shared.bo.Movie;
+import de.hdm.itprojekt.shared.bo.Presentation;
+import de.hdm.itprojekt.shared.bo.Timeslot;
+import de.hdm.itprojekt.shared.bo.User;
+
+public class PresentationNewDialogBox extends DialogBox {
+	
+	AdminAdministrationAsync adminAdministration = ClientSideSettings.getAdminAdministration();
+    
+	VerticalPanel content = new VerticalPanel();
+    HorizontalPanel buttoncontent  = new HorizontalPanel();
+    
+    Label namebox = new Label("Name");
+    TextBox namefield = new TextBox();
+    
+    User user = null;
+    Vector <Cinema> cine = null;
+    Vector <Movie> movie = null;
+    Vector <Timeslot> timeslot = null;
+    
+    Button yes = new Button("save");
+    Button no = new Button("X");
+    
+    ListBox cinemaBox = new ListBox();
+    ListBox movieBox = new ListBox();
+    ListBox timeslotBox = new ListBox();
+    DatePicker datePicker = new DatePicker();
+    
+    Label pcinemaLabel = new Label("Cinema");
+    Label pmovieLabel = new Label("Movie");
+    Label ptimeslotLabel = new Label("Timeslot");
+    Label dateLabel = new Label("Date");
+    
+    public PresentationNewDialogBox (User user) {
+    	this.user = user;
+    }
+    
+    public void onLoad() {
+    	super.onLoad();
+    	buttoncontent.add(no);
+    	no.addClickHandler(new close());
+    	content.add(namebox);
+    	content.add(namefield);
+    	content.add(pcinemaLabel);
+    	content.add(cinemaBox);
+    	content.add(pmovieLabel);
+    	content.add(movieBox);
+    	content.add(dateLabel);
+    	content.add(datePicker);
+    	content.add(ptimeslotLabel);
+    	content.add(timeslotBox);
+    	buttoncontent.add(yes);
+    	yes.addClickHandler(new save());
+    	content.add(buttoncontent);
+    	this.add(content);
+    	
+    	adminAdministration.findAllCinemaByUser(this.user, new AsyncCallback<Vector<Cinema>>() {
+    		
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("was ist falsch geloffen");
+			}
+
+			@Override 
+			public void onSuccess(Vector<Cinema> result) {
+				cinemaBox.addItem("Kein Kino ausgewählt");
+				for (int i = 0; i < result.size(); i++ ) {
+					cinemaBox.addItem(result.elementAt(i).getName());
+					cine = result;
+				}
+			}
+		});
+    	
+    	adminAdministration.getAllMovieByUserID(this.user, new AsyncCallback<Vector<Movie>>() {
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("was ist falsch geloffen");
+			}
+
+			@Override 
+			public void onSuccess(Vector<Movie> result) {
+				movieBox.addItem("Kein Film ausgewählt");
+				for (int i = 0; i < result.size(); i++ ) {
+					movieBox.addItem(result.elementAt(i).getName());
+					movie = result;
+				}
+			}
+		});
+    	
+    	adminAdministration.getAllTimeslotByUserID(this.user, new AsyncCallback<Vector<Timeslot>>() {
+    		
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("was ist falsch geloffen");
+			}
+
+			public void onSuccess(Vector<Timeslot> result) {
+				timeslotBox.addItem("Keine Spielzeit ausgewählt");
+				for (int i = 0; i < result.size(); i++ ) {
+					timeslotBox.addItem(result.elementAt(i).getTime());
+					timeslot = result;
+				}
+			}
+		});    	
+    } 
+
+	/*
+	 * Methode die das Anzeigen der DialogBox realisiert
+	 */
+	public void openPresentation() {
+		this.setGlassEnabled(true);
+		this.setAnimationEnabled(true);
+		this.center();
+		this.show();	
+	}
+	
+	/*
+	 * Methode die das schließen des Fensters realisiert
+	 */
+	public void closePresentation() {
+		this.hide();
+		this.clear();
+		this.removeFromParent();
+		this.setAnimationEnabled(false);
+		this.setGlassEnabled(false);
+	}
+    
+    
+    private class close implements ClickHandler{
+
+		@Override
+		public void onClick(ClickEvent event) {
+			closePresentation();
+		}
+    }
+    
+    private class save implements ClickHandler{
+
+		@Override
+		public void onClick(ClickEvent event) {
+		Cinema c = cine.elementAt(cinemaBox.getSelectedIndex()-1);
+		Movie m = movie.elementAt(movieBox.getSelectedIndex()-1);
+		Timeslot t = timeslot.elementAt(timeslotBox.getSelectedIndex()-1);
+		Date date = new java.sql.Date(datePicker.getValue().getTime());
+		Presentation p = new Presentation(namefield.getText(),c.getId(), m.getId(), user.getId(), t.getId(), date);
+		if(namefield.getText() != null & cinemaBox.getSelectedItemText() != "Kein Kino ausgewählt" & movieBox.getSelectedItemText() !="Kein Film ausgewählt" & timeslotBox.getSelectedItemText() != "Keine Spielzeit ausgewählt") {
+			adminAdministration.addPresentation(p, new AsyncCallback<Presentation>() {
+	
+				@Override
+				public void onFailure(Throwable caught) {
+					// TODO Auto-generated method stub
+					
+				}
+	
+				@Override
+				public void onSuccess(Presentation result) {
+					RootPanel.get().clear();
+					AdminForm adminform = new AdminForm(user,4);
+					RootPanel.get().add(adminform);
+					closePresentation();
+					
+				}
+			});
+		}
+		}
+}
+}
