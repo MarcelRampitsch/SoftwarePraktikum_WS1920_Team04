42937649bad2a8667f7b91e0255ead3bc144c8c0
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-10 22:01:23 +0100

    ListBox und HorizontalPanel hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewForm.java
@@ -1,7 +1,9 @@
 package de.hdm.itprojekt.client.gui;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -21,5 +23,8 @@ public class GroupViewForm extends VerticalPanel {
 	Label surveyNameLabel = new Label("Umfrage:");
 	TextBox surveyNameTB = new TextBox();
 	Button newSurveyButton = new Button("Neu");
+	
+	ListBox memberLB = new ListBox();
+	HorizontalPanel surveyPanel = new HorizontalPanel();
 	
 }
