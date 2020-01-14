40bcc5247e2e49c327ebebc09298938a4ba15a05
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-14 14:29:16 +0100

    Konstruktor hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewCellListForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewCellListForm.java
@@ -1,5 +1,32 @@
 package de.hdm.itprojekt.client.gui;
 
+import java.util.List;
+
+import de.hdm.itprojekt.client.ClientSideSettings;
+import de.hdm.itprojekt.shared.EditorAdministrationAsync;
+import de.hdm.itprojekt.shared.bo.Group;
+import de.hdm.itprojekt.shared.bo.User;
+
+
+/*
+ * 
+ * @author VanDuyHo
+ * 
+ */
+
 public class GroupViewCellListForm {
+	
+	EditorAdministrationAsync editorAdministration = ClientSideSettings.getEditorAdministration();
+	
+	User user = null;
+
+	List<Group> Gruppen;
+	
+	public GroupViewCellListForm(User user, List<Group> Gruppen) {
+		
+		this.user = user;
+		this.Gruppen = Gruppen;
+		
+	}
 
 }
