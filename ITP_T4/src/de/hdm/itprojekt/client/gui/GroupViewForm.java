a5ecda14cd2c253508f9e0bef2edd09fe74d76a1
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-14 16:49:55 +0100

    Diverse Anpassungen vorgenommen

--- ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewForm.java
@@ -6,10 +6,13 @@ import java.util.Vector;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.ListDataProvider;
@@ -18,6 +21,7 @@ import de.hdm.itprojekt.client.ClientSideSettings;
 import de.hdm.itprojekt.shared.EditorAdministrationAsync;
 import de.hdm.itprojekt.shared.bo.Group;
 import de.hdm.itprojekt.shared.bo.Survey;
+import de.hdm.itprojekt.shared.bo.SurveyEntry;
 import de.hdm.itprojekt.shared.bo.User;
 
 
@@ -29,25 +33,31 @@ import de.hdm.itprojekt.shared.bo.User;
 
 public class GroupViewForm extends VerticalPanel {
 
-	EditorAdministrationAsync editorVerwaltung = ClientSideSettings.getEditorAdministration();
+	EditorAdministrationAsync editorAdministration = ClientSideSettings.getEditorAdministration();
 	
 	User user = null;
 	Vector<User> groupMember = new Vector<User>();
+	Vector<SurveyEntry> surveyEntry = new Vector<SurveyEntry>();
 	Group group = null;
+	Survey survey = null;
 	
 	List<Group> Gruppen = null;
 	
+	Label groupLabel = new Label("Gruppen");
 	Label groupNameLabel = new Label("Gruppenname:");
 	TextBox groupNameTB = new TextBox();
-	Label memberNamesLabel = new Label("Mitglieder:");
-	TextBox memberNamesTB = new TextBox();
-	Label surveyNameLabel = new Label("Umfrage:");
+	Label memberNameLabel = new Label("Mitglieder:");
+	TextBox memberNameTB = new TextBox();
+	Label surveyNameLabel = new Label("Umfragen:");
 	TextBox surveyNameTB = new TextBox();
-	Button newSurveyButton = new Button("Neu");
+	Button newSurveyButton = new Button("Neue Umfrage erstellen");
+	Button backButton = new Button("zurück");
 	
 	ListBox memberLB = new ListBox();
+	HorizontalPanel groupNamePanel = new HorizontalPanel();
+	HorizontalPanel memberNamePanel = new HorizontalPanel();
 	HorizontalPanel surveyPanel = new HorizontalPanel();
-	VerticalPanel mainPanel = new VerticalPanel();
+	VerticalPanel contentPanel = new VerticalPanel();
 	
 	List<Survey> Umfragen;
 	
@@ -59,6 +69,14 @@ public class GroupViewForm extends VerticalPanel {
 		
 	}
 	
+	public GroupViewForm(User user, Survey survey, Vector<SurveyEntry> surveyEntry) {
+		
+		this.user = user;
+		this.survey = survey;
+		this.surveyEntry = surveyEntry;
+		
+	}
+	
 	public GroupViewForm() {
 		
 	}
@@ -67,29 +85,57 @@ public class GroupViewForm extends VerticalPanel {
 		
 		super.onLoad();
 		
-		buildForm();
+//		this.clear();
 		
-		final CellTable<Survey> table = new CellTable<Survey>();
+		Grid gvfGrid = new Grid(4, 3);
+		this.add(gvfGrid);
 		
-		ListDataProvider<Survey> dataProvider = new ListDataProvider<Survey>();
+		gvfGrid.setWidget(0, 0, groupLabel);
+		gvfGrid.setWidget(0, 1, backButton);
+		gvfGrid.setWidget(1, 0, groupNameLabel);
+		gvfGrid.setWidget(1, 1, groupNameTB);
+		gvfGrid.setWidget(2, 0, memberNameLabel);
+		gvfGrid.setWidget(2, 1, memberNameTB);
+		gvfGrid.setWidget(3, 0, surveyNameLabel);
+		gvfGrid.setWidget(3, 1, surveyNameTB);
+		gvfGrid.setWidget(3, 2, newSurveyButton);
+		
+		backButton.addClickHandler(new backButtonClickHandler());
+		newSurveyButton.addClickHandler(new newSurveyButtonClickHandler());
+		
+//		groupNamePanel.add(groupNameLabel);
+//		groupNamePanel.add(groupNameTB);
+//		memberNamePanel.add(memberNameLabel);
+//		memberNamePanel.add(memberNameTB);
+//		surveyPanel.add(surveyNameLabel);
+//		surveyPanel.add(surveyNameTB);
+//		surveyPanel.add(newSurveyButton);
+//		contentPanel.add(groupNamePanel);
+//		contentPanel.add(memberNamePanel);
+//		contentPanel.add(surveyPanel);
+
+//		groupNameTB.setText(group.getName()); hier funktioniert was nicht!!!
+
+
+		
+//		this.add(contentPanel);
+		
+//		final CellTable<Survey> table = new CellTable<Survey>();
+		
+//		ListDataProvider<Survey> dataProvider = new ListDataProvider<Survey>();
 		
 	}
 	
-	public void buildForm() {
-		
-		this.clear();
-		
-		mainPanel.add(groupNameLabel);
-		mainPanel.add(groupNameTB);
-		mainPanel.add(memberNamesLabel);
-		mainPanel.add(memberNamesTB);
-		mainPanel.add(surveyPanel);
-		mainPanel.add(newSurveyButton);
-		groupNameTB.setText(group.getName());
-		
-		newSurveyButton.addClickHandler(new newSurveyButtonClickHandler());
-		
-		this.add(mainPanel);
+	private class backButtonClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			RootPanel.get().clear();
+			EditorForm editorForm = new EditorForm(user, Gruppen);
+			RootPanel.get().add(editorForm);
+			
+		}	
 		
 	}
 	
@@ -98,7 +144,7 @@ public class GroupViewForm extends VerticalPanel {
 		@Override
 		public void onClick(ClickEvent event) {
 			
-			
+			Window.alert("Test");
 			
 		}
 		
