8b95fa0c27a44490dcc0f9fa944ac96e77abec98
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-14 14:35:46 +0100

    onLoad-Methode hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewCellListForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/GroupViewCellListForm.java
@@ -1,10 +1,15 @@
 package de.hdm.itprojekt.client.gui;
 
 import java.util.List;
+import java.util.Vector;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.itprojekt.client.ClientSideSettings;
 import de.hdm.itprojekt.shared.EditorAdministrationAsync;
 import de.hdm.itprojekt.shared.bo.Group;
+import de.hdm.itprojekt.shared.bo.Groupmember;
 import de.hdm.itprojekt.shared.bo.User;
 
 
@@ -14,7 +19,7 @@ import de.hdm.itprojekt.shared.bo.User;
  * 
  */
 
-public class GroupViewCellListForm {
+public class GroupViewCellListForm extends VerticalPanel {
 	
 	EditorAdministrationAsync editorAdministration = ClientSideSettings.getEditorAdministration();
 	
@@ -22,11 +27,28 @@ public class GroupViewCellListForm {
 
 	List<Group> Gruppen;
 	
+	Vector<Groupmember> member = new Vector<Groupmember>();
+	Vector<User> groupMember = new Vector<User>();
+	
+	Button newSurvey = new Button("Neue Umfrage erstellen");
+	
+	VerticalPanel contentPanel = new VerticalPanel();
+	
 	public GroupViewCellListForm(User user, List<Group> Gruppen) {
 		
 		this.user = user;
 		this.Gruppen = Gruppen;
 		
 	}
+	
+	public void onLoad() {
+		
+		
+		
+		
+		
+	}
+	
+	
 
 }
