cb4cf2d70dbfa6814693630e6d1c950f0831f4a3
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-23 15:02:41 +0100

    Buttons geändert und ClickHandler hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/client/gui/ToolbarForm.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/ToolbarForm.java
@@ -1,5 +1,7 @@
 package de.hdm.itprojekt.client.gui;
 
+import java.util.List;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
@@ -7,13 +9,16 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 import de.hdm.itprojekt.client.gui.admin.AdminForm;
+import de.hdm.itprojekt.shared.bo.Group;
 import de.hdm.itprojekt.shared.bo.User;
 
 public class ToolbarForm extends HorizontalPanel {
 	
 	User user = null;
-	
+
 	int a = 0;
+	
+	List <Group> Gruppen;
 	
 	public ToolbarForm() {
 		
@@ -26,15 +31,18 @@ public class ToolbarForm extends HorizontalPanel {
 		super.onLoad();
 		
 		Button adminBtn = new Button("Admin");
-		adminBtn.addStyleName("button");
+		adminBtn.addStyleName("toolbar-button");
 		
-		Button createGroupBtn = new Button("Create Group");
-		createGroupBtn.addStyleName("button");
-		
-		Button groupViewBtn = new Button("Group View");
-		groupViewBtn.addStyleName("button");
+		Button editorBtn = new Button("Editor");
+		editorBtn.addStyleName("toolbar-button");
 		
 		adminBtn.addClickHandler(new adminBtnClickHandler());
+		editorBtn.addClickHandler(new editorBtnClickHandler());
+		
+		this.addStyleName("toolbar");
+		
+		this.add(adminBtn);
+		this.add(editorBtn);
 		
 	}
 	
@@ -46,6 +54,19 @@ public class ToolbarForm extends HorizontalPanel {
 			RootPanel.get().clear();
 			AdminForm adminForm = new AdminForm(user, a);
 			RootPanel.get().add(adminForm);
+			
+		}
+			
+	}
+	
+	private class editorBtnClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			
+			RootPanel.get().clear();
+			EditorForm editorForm = new EditorForm(user, Gruppen);
+			RootPanel.get().add(editorForm);
 			
 		}
 			
