155dcc1a8114665d9849675e8484dc5546c8ba2f
Van Duy Ho <v.duy.ho@gmail.com>
2020-01-25 14:22:55 +0100

    LoginForm erstellt

---
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/LoginForm.java
@@ -0,0 +1,47 @@
+package de.hdm.itprojekt.client.gui;
+
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojekt.shared.bo.User;
+
+/*
+ * 
+ * @author VanDuyHo
+ * 
+ */
+public class LoginForm extends VerticalPanel {
+
+	User user = null;
+	Anchor signInLink = new Anchor();
+	Image googleSignIn = new Image ("/images/googleLogin.png");
+	
+	public LoginForm(User user) {
+		
+		this.user = user;
+
+	}
+	
+	public void onLoad() {
+
+		signInLink.setHref(user.getURL());
+		Label welcomeLabel = new Label("Herzlich Willkommen bei Kinofix!");
+		Label txtLabel = new Label("Bitte melden Sie ich sich mit Ihrem Google-Account an, um KinoFix zu nutzen.");
+		HorizontalPanel loginPanel = new HorizontalPanel();
+		welcomeLabel.setStyleName("h1");
+		googleSignIn.setStylePrimaryName("signinwithgoogle");
+		signInLink.getElement().appendChild(googleSignIn.getElement());
+		loginPanel.addStyleName("align-center");
+		this.setHorizontalAlignment(ALIGN_CENTER);
+		this.addStyleName("align-center");
+		loginPanel.add(signInLink);
+		this.add(welcomeLabel);
+		this.add(txtLabel);
+		this.add(loginPanel);
+		
+	}
+
+}
