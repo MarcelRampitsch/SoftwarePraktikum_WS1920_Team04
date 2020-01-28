0183751a7b6f6a6e9f387a3142815491728150a1
MarcelRampitsch <mr164@hdm-stuttgart.de>
2020-01-28 23:55:03 +0100

    Kommentare zum BO Vote hinzugefügt

--- ITP_T4/src/de/hdm/itprojekt/shared/bo/Vote.java
+++ ITP_T4/src/de/hdm/itprojekt/shared/bo/Vote.java
@@ -2,14 +2,37 @@ package de.hdm.itprojekt.shared.bo;
 
 import java.sql.Timestamp;
 
+/*
+ * Realisierung der Vote Klasse. Votes setzen sich aus den Attributen ID, Erstellungsdatum,
+ * UmfrageeintragID, NutzerID und voteErgebnis zusammen. Sie bilden das Wahlverhalten 
+ * der Anwender in Umfragen ab.
+ * 
+ */
 public class Vote extends BusinessObject {
 
+	/*
+	 * SerialVersionUID zur eindeutigen Identifikation der Version der serialisierbaren Klasse.
+	 */
 	private static final long serialversionUID = 1l;
 	
+	/*
+	 * Die UmfrageeintragID zum Vote
+	 */
 	private int surveyentryID;
+	
+	/*
+	 * Die UserID zum Vote
+	 */
 	private int userID;
+	
+	/*
+	 * Das Wahlergebnis
+	 */
 	private int voteResult;
 	
+	/*
+	 * Konstruktoren
+	 */
 	public Vote(int id, Timestamp creationDate, int surveyentryID, int userID, int voteResult) {
 		super(id, creationDate);
 		this.surveyentryID = surveyentryID;
@@ -22,30 +45,51 @@ public class Vote extends BusinessObject {
 		this.voteResult = voteResult;
 	}
 	
+	/*
+	 * Default Konstruktor
+	 */
 	public Vote() {
 		
 	}
 	
+	/*
+	 * Auslesen des Wahlergebnis.
+	 */
 	public int getVoteResult() {
 		return voteResult;
 	}
 
+	/*
+	 * Setzen des Wahlergebnis.
+	 */
 	public void setVoteResult(int voteResult) {
 		this.voteResult = voteResult;
 	}
 
+	/*
+	 * Auslesen der UmfrageeintragID
+	 */
 	public int getSurveyEntryID() {
 		return surveyentryID;
 	}
 	
+	/*
+	 * Setzen der UmfrageeintragID
+	 */
 	public void setSurveyEntryID(int surveyentryID) {
 		this.surveyentryID = surveyentryID;
 	}
 	
+	/*
+	 * Auslesen der UserID
+	 */
 	public int getUserID() {
 		return userID;
 	}
 	
+	/*
+	 * Setzen der UserID
+	 */
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
