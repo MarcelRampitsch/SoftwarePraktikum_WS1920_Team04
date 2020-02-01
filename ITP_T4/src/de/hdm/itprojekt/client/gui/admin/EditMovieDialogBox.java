a0ceedaa4b4a94a0fc3b5f515ade63a053490708
BastianGroezinger <bg036@hdm-stuttgart.de>
2020-02-01 13:24:46 +0100

    Zeichkette verlängern

--- ITP_T4/src/de/hdm/itprojekt/client/gui/admin/EditMovieDialogBox.java
+++ ITP_T4/src/de/hdm/itprojekt/client/gui/admin/EditMovieDialogBox.java
@@ -133,7 +133,7 @@ public class EditMovieDialogBox extends DialogBox {
 		 box.setFocus(true);
 
 	     // Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
-	     if (!symbol.matches("^[0-9A-Z\\\\.]{1,10}$")) {
+	     if (!symbol.matches("^[0-9A-Z\\\\.]{1,20}$")) {
 	       box.selectAll();
 	       return 1;
 	     }
